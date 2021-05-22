b0aa44fa690c861e379932e5c5baf40b01a323fa
MaksMelnyk <maks.melnik1611@gmail.com>
2021-05-22 20:34:33 +0300

    implement useHttp hook. Implement registartion request

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json());
+app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth.routes'));
 
